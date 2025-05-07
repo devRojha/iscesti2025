b41efe5525778b26530f8e5fa3a384c27cde3d19
Devraj Kumar <devraj227804@gmail.com>
2025-05-07 23:57:34 +0530

    add schedule

--- components/Notice.tsx
+++ components/Notice.tsx
@@ -18,6 +18,7 @@ export default function Notice(){
                 <div className="text-2xl font-bold text-center p-2 font-serif">Notice</div>
                 <div className="h-[350px]  p-3 pt-4">
                     <ul className={`list-disc ml-4 space-y-2 underline `}>
+                    <li className={`${(col)? "text-blue-600" :"text-yellow-600"}`}><a href="/Schedule_of_Presentation_ISCESTI_2025.pdf" target="blank">Schedule of Presentation</a></li>
                     <li className={`${(col)? "text-blue-600" :"text-yellow-600"}`}><a href="/Full_paper_template.docx" target="blank">Download Full Paper Template</a></li>
                     <li className={`${(col)? "text-blue-600" :"text-yellow-600"}`}><a href="https://docs.google.com/forms/d/e/1FAIpQLSd9gVbCyeuhoQIRheN6p7SSIFiHYTb_3oRTXemjr20XpEBW8g/viewform" target="blank">Registration Link</a></li>
                     <li className={`${(col)? "text-blue-600" :"text-yellow-600"}`}><a href="https://cmt3.research.microsoft.com/User/Login?ReturnUrl=%2FISCESTI2025%2FSubmission%2FManage" target="blank">Paper Submission Link</a></li>
--- components/SlidingNotice.tsx
+++ components/SlidingNotice.tsx
@@ -4,7 +4,7 @@ export default function SlidingNotice(){
     return (
         <div className="h-40 max-sm:h-20 relative flex flex-col justify-center bg-orange-50 text-black overflow-hidden z-0">
             <div className="absolute animate-slide overflow-auto text-red-900 flex">
-                <div className="mr-20">Registration Link is Live now :  <a href="https://docs.google.com/forms/d/e/1FAIpQLSd9gVbCyeuhoQIRheN6p7SSIFiHYTb_3oRTXemjr20XpEBW8g/viewform" className="underline text-blue-700" target="blank">click here</a></div>
+                <div className="mr-20">Schedule of Presentation :  <a href="/Schedule_of_Presentation_ISCESTI_2025.pdf" className="underline text-blue-700" target="blank">click here</a></div>
                 <div>Paper Submission Link :  <a href="https://cmt3.research.microsoft.com/User/Login?ReturnUrl=%2FISCESTI2025%2FSubmission%2FManage" className="underline text-blue-700" target="blank">click here</a></div>
             </div>
             <div className="max-sm:hidden grid grid-cols-8 h-full">
